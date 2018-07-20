330afd42dcdba3e0b70931d2db07463bebe4ce78
Maria Pospelova <mariyapospelova@gmail.com>
2018-07-20 12:05:09 +0200

    Update: new directory in assemblies and renamed icons + images

---
+++ common/assemblies/noshadow/icons/make_icons.sh
@@ -0,0 +1,4 @@
+size = 64x64
+
+convert ../600x600_transp/Sphere_transp.png -resize $size ff_Sphere_$size.png
+convert ../600x600_transp/MesoCrystal_transp.png -resize $size ff_MesoCrystal_$size.png
--- common/formfactors/noshadow/icons/make_icons.sh
+++ common/formfactors/noshadow/icons/make_icons.sh
@@ -2,28 +2,25 @@
 
 size=64x64
 
-#convert ../600x600_transp/AnisoPyramid_transp.png -resize $size ff_AnisoPyramid_$size.png 
-#convert ../600x600_transp/Box_transp.png -resize $size ff_Box_$size.png
-#convert ../600x600_transp/Cone_transp.png -resize $size ff_Cone_$size.png
-#convert ../600x600_transp/Cone6_transp.png -resize $size ff_Cone6_$size.png
-#convert ../600x600_transp/Cuboctahedron_transp.png -resize $size ff_Cuboctahedron_$size.png
-#convert ../600x600_transp/Cylinder_transp.png -resize $size ff_Cylinder_$size.png
-#convert ../600x600_transp/Dodecahedron_transp.png -resize $size ff_Dodecahedron_$size.png
-#convert ../600x600_transp/Dot_transp.png -resize $size ff_Dot_$size.png
-#convert ../600x600_transp/EllipsoidalCylinder_transp.png -resize $size ff_EllipsoidalCylinder_$size.png
-#convert ../600x600_transp/FullSphere_transp.png -resize $size ff_FullSphere_$size.png
-#convert ../600x600_transp/FullSpheroid_transp.png -resize $size ff_FullSpheroid_$size.png
-#convert ../600x600_transp/HemiEllipsoid_transp.png -resize $size ff_HemiEllipsoid_$size.png 
-#convert ../600x600_transp/Icosahedron_transp.png -resize $size ff_Icosahedron_$size.png
-#convert ../600x600_transp/Prism3_transp.png -resize $size ff_Prism3_$size.png
-#convert ../600x600_transp/Prism6_transp.png -resize $size ff_Prism6_$size.png
-#convert ../600x600_transp/Pyramid_transp.png -resize $size ff_Pyramid_$size.png
-#convert ../600x600_transp/Ripple1_transp.png -resize $size ff_Ripple1_$size.png
-#convert ../600x600_transp/Ripple6_transp.png -resize $size ff_Ripple2_$size.png
-#convert ../600x600_transp/Tetrahedron_transp.png -resize $size ff_Tetrahedron_$size.png
-#convert ../600x600_transp/TruncatedCube_transp.png -resize $size ff_TruncatedCube_$size.png
-#convert ../600x600_transp/TruncatedSphere_transp.png -resize $size ff_TruncatedSphere_$size.png
-#convert ../600x600_transp/TruncatedSpheroid_transp.png -resize $size ff_TruncatedSpheroid_$size.png
-
-convert ../600x600_transp/Sphere_transp.png -resize $size ff_Sphere_$size.png
-convert ../600x600_transp/MesoCrystal_transp.png -resize $size ff_MesoCrystal_$size.png
+convert ../600x600_transp/AnisoPyramid_transp.png -resize $size ff_AnisoPyramid_$size.png 
+convert ../600x600_transp/Box_transp.png -resize $size ff_Box_$size.png
+convert ../600x600_transp/Cone_transp.png -resize $size ff_Cone_$size.png
+convert ../600x600_transp/Cone6_transp.png -resize $size ff_Cone6_$size.png
+convert ../600x600_transp/Cuboctahedron_transp.png -resize $size ff_Cuboctahedron_$size.png
+convert ../600x600_transp/Cylinder_transp.png -resize $size ff_Cylinder_$size.png
+convert ../600x600_transp/Dodecahedron_transp.png -resize $size ff_Dodecahedron_$size.png
+convert ../600x600_transp/Dot_transp.png -resize $size ff_Dot_$size.png
+convert ../600x600_transp/EllipsoidalCylinder_transp.png -resize $size ff_EllipsoidalCylinder_$size.png
+convert ../600x600_transp/FullSphere_transp.png -resize $size ff_FullSphere_$size.png
+convert ../600x600_transp/FullSpheroid_transp.png -resize $size ff_FullSpheroid_$size.png
+convert ../600x600_transp/HemiEllipsoid_transp.png -resize $size ff_HemiEllipsoid_$size.png 
+convert ../600x600_transp/Icosahedron_transp.png -resize $size ff_Icosahedron_$size.png
+convert ../600x600_transp/Prism3_transp.png -resize $size ff_Prism3_$size.png
+convert ../600x600_transp/Prism6_transp.png -resize $size ff_Prism6_$size.png
+convert ../600x600_transp/Pyramid_transp.png -resize $size ff_Pyramid_$size.png
+convert ../600x600_transp/Ripple1_transp.png -resize $size ff_Ripple1_$size.png
+convert ../600x600_transp/Ripple6_transp.png -resize $size ff_Ripple2_$size.png
+convert ../600x600_transp/Tetrahedron_transp.png -resize $size ff_Tetrahedron_$size.png
+convert ../600x600_transp/TruncatedCube_transp.png -resize $size ff_TruncatedCube_$size.png
+convert ../600x600_transp/TruncatedSphere_transp.png -resize $size ff_TruncatedSphere_$size.png
+convert ../600x600_transp/TruncatedSpheroid_transp.png -resize $size ff_TruncatedSpheroid_$size.png
