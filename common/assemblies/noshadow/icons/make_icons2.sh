784469daf76d400f95c76340ec6cdf668f2fa1de
Maria Pospelova <mariyapospelova@gmail.com>
2018-07-24 10:04:57 +0200

    New assemblies: DistributedParticle

--- common/assemblies/noshadow/icons/make_icons2.sh
+++ common/assemblies/noshadow/icons/make_icons2.sh
@@ -1,6 +1,7 @@
 
 size=64x64
 
-convert ../600x600_transp/ParticleCoreShell_transp.png -resize $size ff_ParticleCoreShell_$size.png 
-convert ../600x600_transp/ParticleComposition_transp.png -resize $size ff_ParticleComposition_$size.png
-convert ../600x600_transp/Mesocrystal_transp.png -resize $size ff_Mesocrystal_$size.png
+#convert ../600x600_transp/ParticleCoreShell_transp.png -resize $size ff_ParticleCoreShell_$size.png 
+#convert ../600x600_transp/ParticleComposition_transp.png -resize $size ff_ParticleComposition_$size.png
+#convert ../600x600_transp/Mesocrystal_transp.png -resize $size ff_Mesocrystal_$size.png
+convert ../600x600_transp/DistributedParticle_transp.png -resize $size ff_DistributedParticle_$size.png
